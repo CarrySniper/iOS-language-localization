2ddd7134ae3f5fb32ea6c3ce8cfda7734c93416e
cjq002 <CK_chan@cjq.local>
2015-11-08 11:02:27 +0800

    Initial Commit

---
+++ 本地化语言和切换/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  本地化语言和切换
+//
+//  Created by CK_chan on 15/11/8.
+//  Copyright © 2015年 CK_chan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 本地化语言和切换/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  本地化语言和切换
+//
+//  Created by CK_chan on 15/11/8.
+//  Copyright © 2015年 CK_chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
